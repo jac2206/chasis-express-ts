cad41a925b924dc5134095b6f9b799ed4f7a4884
jac2206 <arango773@gmail.com>
2026-08-26 16:42:06 -0500

    docs: docs yess

--- src/infraestructure/http/routes/v1/index.ts
+++ src/infraestructure/http/routes/v1/index.ts
@@ -3,7 +3,7 @@ import genericRoutes from './generic.routes';
 import usersRoutes from './users.routes';
 
 const router = Router();
-
+//probar
 router.use('/generic', genericRoutes);
 router.use('/users', usersRoutes);
 
